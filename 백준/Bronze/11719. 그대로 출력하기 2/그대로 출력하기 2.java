b4f02837aa2e0a1d9137cf444d31072e3df06e63
JaeHyeon Son <sjh9391985@gmail.com>
2023-04-24 13:42:39 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 128 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,24 @@
+/*
+    Hello
+
+Baekjoon     
+   Online Judge    
+*/
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = "";
+        
+        while((str = br.readLine()) != null){
+            System.out.println(str);
+        }
+        
+    }
+    
+}
