9ebe79295b74ac1987380fc7bda4fbaf12327573
JaeHyeon Son <sjh9391985@gmail.com>
2023-04-24 17:11:07 +0900

    [Bronze III] Title: 최소, 최대, Time: 1316 ms, Memory: 103472 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -3,19 +3,21 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		int index = 0;
-		int[] arr = new int[N];
-		while(st.hasMoreTokens()) {
-			arr[index] = Integer.parseInt(st.nextToken());
-			index++;
-		}
-		
-		Arrays.sort(arr);
-		System.out.print(arr[0] + " " + arr[N - 1]);
+        int length = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int[] nums = new int[length];
+        
+        int index = 0;
+        while(st.hasMoreTokens()){
+            nums[index] = Integer.parseInt(st.nextToken());
+            index++;
+        }
+        
+        Arrays.sort(nums);
+        System.out.println(nums[0]+ " " + nums[length-1]);
     }
 }
