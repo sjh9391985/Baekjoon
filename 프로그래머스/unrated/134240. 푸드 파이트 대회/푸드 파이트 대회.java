89357fc7f2ba7bf9299f8435762b06e272291bfb
JaeHyeon Son <sjh9391985@gmail.com>
2023-05-02 20:05:49 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 10.68 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < food.length; i++){
+            int division = food[i] / 2;
+            for(int j = 0; j < division; j++) {
+                sb.append(i);
+            }
+        }
+        
+        
+        return sb.toString() + "0" + sb.reverse().toString();
+    }
+}
