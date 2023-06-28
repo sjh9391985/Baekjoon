0996d8443f7eca56f262c3b249e1919363082d9d
JaeHyeon Son <sjh9391985@gmail.com>
2023-06-28 16:27:21 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.07 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        StringBuffer sf = new StringBuffer(my_string);
+        return sf.reverse().toString();
+    }
+}
