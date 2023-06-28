024616d1e5969be9502b2f5f2b6bb645ebe3cb93
JaeHyeon Son <sjh9391985@gmail.com>
2023-06-28 16:06:40 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.03 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        int count = 0;
+        for(int i : numbers){
+            answer[count] = i * 2;
+            count++;
+        }
+        return answer;
+    }
+}
