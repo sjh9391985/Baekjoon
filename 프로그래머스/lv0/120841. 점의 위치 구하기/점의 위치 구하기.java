bc1583433f4a0ba5e3fe2c890f1526fb8388bc92
JaeHyeon Son <sjh9391985@gmail.com>
2023-07-03 10:02:18 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] dot) {
+        int x = dot[0];
+        int y = dot[1];
+        
+        return      x > 0 && y > 0 
+                ? 1 
+                :   x > 0 && y < 0
+                ? 4
+                :   x < 0 && y > 0
+                ? 2
+                :   x < 0 && y < 0
+                ? 3
+                : 0;
+    }
+}
