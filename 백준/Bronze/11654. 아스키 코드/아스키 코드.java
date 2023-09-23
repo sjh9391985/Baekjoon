4ff7f230ed2ec4255dafdad86a7f1ae160cf4b5e
JaeHyeon Son <sjh9391985@gmail.com>
2023-09-23 12:04:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 212 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        char ac = sc.next().charAt(0);
+        System.out.print(Integer.valueOf(ac));
+        
+    }
+}
