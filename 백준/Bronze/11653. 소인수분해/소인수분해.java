49e4e8816fc5bbb064585c243bfd018ac9eeaf13
JaeHyeon Son <sjh9391985@gmail.com>
2024-01-30 09:20:57 +0900

    [Bronze I] Title: 소인수분해, Time: 252 ms, Memory: 17948 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int i = 2;
+
+        while (N >= i) {
+            if(N % i == 0){
+                System.out.println(i);
+                N /= i;
+            } else {
+                i++;
+            }
+        }
+
+    }
+
+}
